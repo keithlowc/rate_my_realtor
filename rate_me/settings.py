04f7d1d9a619c7f91ff553e64c600d6289ab4b75
Keith Low <keithlowc@gmail.com>
2019-06-27 23:56:14 -0400

    added rate_me/static

--- rate_me/settings.py
+++ rate_me/settings.py
@@ -138,7 +138,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, '../templates/static/')
+STATIC_ROOT = os.path.join(BASE_DIR, '../rate_me/static/')
 STATICFILES_DIRS = [
     BASE_DIR + '/rate_me/static',
 ]

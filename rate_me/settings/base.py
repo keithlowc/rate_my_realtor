9dcd93b8ab98a4df277ab8cf15991d34659b7043
Keith Low <keithlowc@gmail.com>
2019-06-30 09:41:08 -0400

    fixed css and style for admin page

--- rate_me/settings/base.py
+++ rate_me/settings/base.py
@@ -140,7 +140,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, '../rate_me/static/')
 STATICFILES_DIRS = [
-    BASE_DIR + '/rate_me/static',
+    BASE_DIR + '/static/admin',
 ]
 
 # Crispy forms
